446e5ca1d7b35a86c336d45284565047f89e9cd2
strongreasons <abenkenari@gmail.com>
2025-02-27 10:24:03 +0000

    X00TD: Evolution-X bringup
    Change-Id: I636f814858d39b0d018d7daebe952d618bd6f8b6

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -12,6 +12,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_DISABLE_EPPE := true
+
+# Boot animaton
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
@@ -25,9 +30,6 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=ASUS_X00T \
-    PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 1309 release-keys"
-
-# Build fingerprint
-BUILD_FINGERPRINT := "asus/WW_X00TD/ASUS_X00T_2:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys"
+    DeviceProduct=ASUS_X00TD \
+    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
+    BuildFingerprint=asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
---
+++ vendorsetup.sh
@@ -0,0 +1,21 @@
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
+rm -rf kernel/asus/sdm660/KernelSU-Next/website
+rm -rf kernel/asus/sdm660/KernelSU-Next/scripts
+rm -rf kernel/asus/sdm660/KernelSU-Next/docs
+
+rm -rf device/asus
+git clone --depth=1 https://github.com/electrolaboratory/common -b vic device/asus/sdm660-common
+
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/electrolaboratory/vendor_common -b vic vendor/asus/sdm660-common
+git clone --depth=1 https://github.com/electrolaboratory/vendor_device -b vic vendor/asus/X00TD
+
+rm -rf vendor/lineage-priv/keys
+mkdir -p vendor/lineage-priv/keys
+git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys/
+
+export WITH_GMS=false
+export KBUILD_BUILD_USER=queen
+export TZ=Asia/Jakarta
