6c7b1dcc86f93013be630eb37a72dfc0c8559939
Meisie Widuri <dotkit@electrowizard.me>
2025-03-05 05:13:38 +0700

    X00TD: Change common tree
    Also use sign key by default source side!

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,14 +7,14 @@ rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 sed -i 's/CONFIG_DEBUG_INFO=.*/CONFIG_DEBUG_INFO=n/g' kernel/asus/sdm660/arch/arm64/configs/asus/X00TD_defconfig
 
 rm -rf device/asus/sdm660-common
-git clone --depth=1 https://github.com/electrolaboratory/common -b v device/asus/sdm660-common
+git clone --depth=1 https://github.com/Tiktodz/android_device_asus_sdm660-common-4.19 device/asus/sdm660-common
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/electrolaboratory/vendor_common -b v vendor/asus
 
 rm -rf vendor/lineage-priv/keys
-git clone https://github.com/ardia-kun/vendor -b 14-backup sig && cp -R sig/* vendor/
-rm -rf sig
+#git clone https://github.com/ardia-kun/vendor -b 14-backup sig && cp -R sig/* vendor/
+#rm -rf sig
 
 export BUILD_USER=queen
 export KBUILD_BUILD_USER=queen
