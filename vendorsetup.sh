4497c690162c56718eb927bd09d1916bc24df92c
Hans98 <142167463+Hans982@users.noreply.github.com>
2025-03-13 05:20:58 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,12 +3,12 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 rm -rf device/asus
-git clone --depth=1 https://github.com/electrolaboratory/common -b los device/asus/sdm660-common
-git clone --depth=1 https://github.com/electrolaboratory/device -b vic device/asus/X00TD
+git clone --depth=1 https://github.com/Hans982/common -b aosp device/asus/sdm660-common
+git clone --depth=1 https://github.com/Hans982/device -b infinity device/asus/X00TD
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/electrolaboratory/vendor_common -b los vendor/asus/sdm660-common
-git clone --depth=1 https://github.com/electrolaboratory/vendor_device -b vic vendor/asus/X00TD
+git clone --depth=1 https://github.com/Hans982/vendor_device -b v vendor/asus/X00TD
 
 rm -rf vendor/lineage-priv/keys
 mkdir -p vendor/lineage-priv/keys
@@ -17,5 +17,5 @@ git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/k
 rm -rf vendor/gms
 git clone https://gitlab.com/dark.phnx12/vendor-gms-matrixx.git vendor/gms
 
-export KBUILD_BUILD_USER=queen
+export KBUILD_BUILD_USER=hans982
 export TZ=Asia/Jakarta
